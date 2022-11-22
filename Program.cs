dc654a1ff4224aaecffd939e681450ac6271f9f1
Ricardo Gomes <ricardokevi@gmail.com>
2022-11-22 18:13:28 -0300

    Program of Orders

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Orders.Entities;
+using Orders.Entities.Enums;
+using System.Collections.Generic;
+
+namespace Orders
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Birth data (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int quantityItemOrder = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= quantityItemOrder; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine());
+
+                Console.Write("Quantity: ");
+                int productQuantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(productName, productPrice);
+                OrderItem orderItem = new OrderItem(productQuantity, productPrice, product);
+
+                order.AddItem(orderItem);               
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+
+        }
+    }
+}
